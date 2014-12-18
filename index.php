14f8e2ec8d4220e6b3d20a2e00eb34a15d705fc2
Jonatan <JLN13010@student.mdh.se>
2014-12-18 13:08:46 +0100

    Set up general filestructure

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
         <title>Bestest webbpage</title>
     </head>
     <body>
-                <h1>Sprängburk!</h1>
+        <h1>Sprängburk!</h1>
         <p>Detta är en sån där mening!! huehue</p>
         <?php echo "jah är inte turk turk"?>
     </body>
